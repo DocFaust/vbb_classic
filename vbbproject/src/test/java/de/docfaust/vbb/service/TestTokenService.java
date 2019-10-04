6a5587074dfd64aaa17cb2c46b10e9694d43dd61
Werner Faust <docfaust75@gmail.com>
2019-10-04 16:13:36 +0200

    repair

--- vbbproject/src/test/java/de/docfaust/vbb/service/TestTokenService.java
+++ vbbproject/src/test/java/de/docfaust/vbb/service/TestTokenService.java
@@ -4,27 +4,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import de.docfaust.vbb.ServiceCreator;
 import de.docfaust.vbb.data.entity.Token;
 import de.docfaust.vbb.data.entity.ValidityState;
-import de.docfaust.vbb.data.facades.ConfigFacade;
-import de.docfaust.vbb.data.facades.TokenFacade;
 import de.docfaust.vbb.data.util.JpaBaseRolledBackTestCase;
 
 class TestTokenService extends JpaBaseRolledBackTestCase {
 
+	private TokenService ts;
+	
+	@BeforeEach
+	public void init()
+	{
+		ServiceCreator sc = new ServiceCreator(em);
+		this.ts = sc.getTokenService();
+	}
 	@Test
 	void testCreateTokenService() {
-		TokenService ts = new TokenServiceImpl();
 		assertThat(ts).isNotNull();
 	}
 	
 	@Test
 	void testCreateToken() {
-		TokenFacade tf = new TokenFacade(em);
-		ConfigFacade cf = new ConfigFacade(em);
-		TokenService ts = new TokenServiceImpl(tf, cf);
 		boolean aFound = false;
 		boolean nineFound = false;
 		for (int i = 0; i < 100; i++) {
@@ -45,9 +49,6 @@ class TestTokenService extends JpaBaseRolledBackTestCase {
 	
 	@Test
 	void testInvalidateToken() {
-		TokenFacade tf = new TokenFacade(em);
-		ConfigFacade cf = new ConfigFacade(em);
-		TokenService ts = new TokenServiceImpl(tf, cf);
 		List<Token> tokens = ts.getTokens();
 		assertThat(tokens).isNotNull();
 		assertThat(tokens).isNotEmpty();
@@ -55,14 +56,11 @@ class TestTokenService extends JpaBaseRolledBackTestCase {
 		assertThat(token.getState()).isEqualTo(ValidityState.VALID);
 		ts.invalidateToken(token);
 		assertThat(token.getState()).isEqualTo(ValidityState.INVALID);
-		assertThat(tf.find(token.getId()).getState()).isEqualTo(ValidityState.INVALID);
+		assertThat(facadenFactory.getTokenFacade().find(token.getId()).getState()).isEqualTo(ValidityState.INVALID);
 	}
 	
 	@Test
 	void testValidateValidToken() {
-		TokenFacade tf = new TokenFacade(em);
-		ConfigFacade cf = new ConfigFacade(em);
-		TokenService ts = new TokenServiceImpl(tf, cf);
 		List<Token> tokens = ts.getTokens();
 		assertThat(tokens).isNotNull();
 		assertThat(tokens).isNotEmpty();
@@ -73,9 +71,6 @@ class TestTokenService extends JpaBaseRolledBackTestCase {
 	
 	@Test
 	void testValidateInvalidToken() {
-		TokenFacade tf = new TokenFacade(em);
-		ConfigFacade cf = new ConfigFacade(em);
-		TokenService ts = new TokenServiceImpl(tf, cf);
 		List<Token> tokens = ts.getTokens();
 		assertThat(tokens).isNotNull();
 		assertThat(tokens).isNotEmpty();
@@ -87,23 +82,17 @@ class TestTokenService extends JpaBaseRolledBackTestCase {
 	
 	@Test
 	void testDeleteToken() {
-		TokenFacade tf = new TokenFacade(em);
-		ConfigFacade cf = new ConfigFacade(em);
-		TokenService ts = new TokenServiceImpl(tf, cf);
 		List<Token> tokens = ts.getTokens();
 		assertThat(tokens).isNotNull();
 		assertThat(tokens).isNotEmpty();
 		assertThat(tokens).hasSize(2);
 		Token token = tokens.get(0);
 		ts.deleteToken(token);
-		assertThat(tf.count()).isEqualTo(1);
+		assertThat(facadenFactory.getTokenFacade().count()).isEqualTo(1);
 	}
 	
 	@Test
 	void testGenerateTokenURL() {
-		TokenFacade tf = new TokenFacade(em);
-		ConfigFacade cf = new ConfigFacade(em);
-		TokenService ts = new TokenServiceImpl(tf, cf);
 		String url = ts.generateTokenURL("1");
 		assertThat(url).isEqualTo("http://localhost:8080/vbb/faces/saldo.xhtml?token=1");		
 	}
