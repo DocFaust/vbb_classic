41e6fe0f7f0c34e2bd0f48945d43458dc9794685
wfa339 <wfa339@SO-NB683.supplyon.de>
2019-10-07 17:35:06 +0200

    coverage

--- vbbproject/src/test/java/de/docfaust/vbb/service/TestSaisonService.java
+++ vbbproject/src/test/java/de/docfaust/vbb/service/TestSaisonService.java
@@ -2,12 +2,18 @@ package de.docfaust.vbb.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import de.docfaust.vbb.ServiceCreator;
 import de.docfaust.vbb.data.entity.Season;
 import de.docfaust.vbb.data.util.JpaBaseRolledBackTestCase;
+import de.docfaust.vbb.util.EntityFactory;
 import de.docfaust.vbb.util.statusliste.Statusliste;
 
 public class TestSaisonService extends JpaBaseRolledBackTestCase {
@@ -15,10 +21,20 @@ public class TestSaisonService extends JpaBaseRolledBackTestCase {
 
 	@BeforeEach
 	public void setUp() {
-		ServiceCreator sc = new ServiceCreator(em);
-		seasonService = sc.getSeasonService();
+		seasonService = new ServiceCreator(em).getSeasonService();
 	}
 
+	@Test
+	public void testInit() {
+		assertThat(new SeasonServiceImpl()).isNotNull();
+
+	}
+	@Test
+	public void testGetSeasons() {
+		int count = facadenFactory.getSeasonFacade().count();
+		assertThat(seasonService.getSeasons()).isNotNull().isNotEmpty().hasSize(count);
+	}
+	
 	@Test
 	public void testDeleteSaison() {
 		int count = facadenFactory.getSeasonFacade().count();
@@ -27,5 +43,27 @@ public class TestSaisonService extends JpaBaseRolledBackTestCase {
 		assertThat(statusliste.booleanValue()).isTrue();
 		assertThat(facadenFactory.getSeasonFacade().count()).isEqualTo(count - 1);
 	}
+	@Test
+	void testGetSeason() {
+		Date from = Date.from(LocalDate.of(2015, 1, 8).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
 
+		Season season = seasonService.getSeason(from);
+		
+		assertThat(season).isNotNull().extracting("description").contains("Saison 3");
+	}
+	
+	@Test
+	void testRemoveOldSeasons() {
+		seasonService.removeOldSeasons(new Date());
+		assertThat(facadenFactory.getSeasonFacade().count()).isEqualTo(0);
+	}
+	
+	@Test
+	void testSaveSeason() {
+		int count = facadenFactory.getSeasonFacade().count();
+		
+		Season season = EntityFactory.createSeason(new Date(), new Date(), BigDecimal.ONE, "Hurra");
+		seasonService.saveSeason(season);
+		assertThat(facadenFactory.getSeasonFacade().count()).isEqualTo(++count);
+	}
 }
